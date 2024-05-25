27717570d5c01a0057589779bf880049d243908b
Akhystophane <emnl.busi@outlook.fr>
2024-05-25 09:38:21 +0200

    Add STATIC_ROOT to settings.py

--- AstroNomos/AstroNomos/settings.py
+++ AstroNomos/AstroNomos/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
@@ -39,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'newsletter',
 ]
+
+import django_on_heroku
+django_on_heroku.settings(locals())
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
