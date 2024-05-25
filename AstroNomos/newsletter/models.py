b38e219e9358a7c65327e94c31afd43df376e84c
Akhystophane <emnl.busi@outlook.fr>
2024-05-25 17:05:19 +0200

    Add e_mail field to Customer model

--- AstroNomos/newsletter/models.py
+++ AstroNomos/newsletter/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=200)
+    e_mail = models.EmailField(max_length=200)
 
     def __str__(self):
         return self.first_name
