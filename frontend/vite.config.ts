c2bd2ae41d007aad12c076cd0918ef914852f30f
Akhystophane <emnl.busi@outlook.fr>
2024-09-29 16:44:05 -0400

    Message décrivant les modifications

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/static/', // Chemin de base pour servir les fichiers
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    assetsDir: 'assets', // Assurez-vous que les fichiers sont dans le dossier `assets`
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
+        assetFileNames: 'assets/[name][extname]'  // Conserve le nom des fichiers sans hash
       }
     }
   }
